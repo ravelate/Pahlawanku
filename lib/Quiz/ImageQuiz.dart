8f15398dc9f3bb637cc4b409de97fb1bde51f06a
Fian Ardi Saputra <55016374+ravelate@users.noreply.github.com>
2021-05-17 10:33:45 +0700

    Add files via upload

---
+++ lib/Quiz/ImageQuiz.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class ImageQuiz extends StatefulWidget {
+  _ImageQuizState createState() => _ImageQuizState();
+}
+class _ImageQuizState extends State<ImageQuiz>{
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/Quiz/TextQuiz.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class TextQuiz extends StatefulWidget {
+  _TextQuizState createState() => _TextQuizState();
+}
+class _TextQuizState extends State<TextQuiz>{
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+}
